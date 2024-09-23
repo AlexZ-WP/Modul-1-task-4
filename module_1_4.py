955c9ce1fa541567a34f8cd67e73b651e7455151
AlexZ-WP <alexz10@mail.ru>
2024-09-23 15:01:15 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Напишите что-нибудь: ')
+print('Количество символов: ', len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[0])
+print(my_string[-1])
+print(my_string.replace(' ', ''))
+
